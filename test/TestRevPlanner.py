4da5e8334c9eead223f3be5e720f53f3be9ae3d0
Archanciel <jp.schnyder@gmail.com>
2017-08-09 23:14:46 +0200

    renamed file

--- test/TestRevPlanner.py
+++ test/TestRevPlanner.py
@@ -5,7 +5,7 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir) 
 
-from RevPlanner import RevPlanner
+from revplanner import RevPlanner
 
 class TestRevPlanner(unittest.TestCase):
 	def testPlanRevNormalData(self):
